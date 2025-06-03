cdb5b46bd2c114291e62b0ce8152c5e2ddbd1a90
valennesci <valen52nesci@gmail.com>
2025-06-03 13:23:31 -0300

    Estados

--- findamatch/src/main/java/com/findamatch/model/Partido.java
+++ findamatch/src/main/java/com/findamatch/model/Partido.java
@@ -1,9 +1,12 @@
 package com.findamatch.model;
 
-import java.util.Date;
-import java.util.List;
-import java.util.ArrayList;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.findamatch.model.emparejamiento.IEstrategiaEmparejamiento;
+import com.findamatch.model.estado.EstadoCreado;
+import com.findamatch.model.estado.IEstadoPartido;
 
 public class Partido {
     private Deporte deporte;
@@ -11,59 +14,108 @@ public class Partido {
     private int duracion;
     private Ubicacion ubicacion;
     private LocalDateTime fecha;
-    // private IEstadoPartido estado;
-    // private IEstrategiaEmparejamiento estrategiaEmparejamiento;
+    private IEstadoPartido estado;
+    private IEstrategiaEmparejamiento estrategiaEmparejamiento;
 
     public Partido(
             Deporte deporte,
-            // List<Usuario> jugadores,
             Ubicacion ubicacion,
             LocalDateTime fecha,
             int duracionMinutos) {
-        // String estado,
-        // String estrategiaEmparejamiento) {
+
         this.deporte = deporte;
-        // this.jugadores = jugadores;
+        this.jugadores = new ArrayList<>();
         this.ubicacion = ubicacion;
         this.fecha = fecha;
         this.duracion = duracionMinutos;
-        // this.estado = estado;
-        // this.estrategiaEmparejamiento = estrategiaEmparejamiento;
+        this.estado = new EstadoCreado(); // Estado inicial
     }
 
-    public String toString() {
-        return "Partido{" +
-                "deporte=" + deporte +
-                // ", jugadores=" + jugadores +
-                ", ubicacion='" + ubicacion + '\'' +
-                ", comienzo=" + fecha +
-                ", duracionMinutos=" + duracion +
-                // ", estado='" + estado + '\'' +
-                // ", estrategiaEmparejamiento='" + estrategiaEmparejamiento + '\'' +
-                '}';
-    }
-
-    // public void cambiarEstado(IEstadoPartido nuevoEstado) {}
-
-    // public void cambiarEstrategia(IEstrategiaEmparejamiento estrategia) {}
-
-    public void crearPartido() {
-
-    }
-
-    public void buscarPartido() {
-
-    }
-
-    public void agregarPartido() {
-
+    // --------- MÉTODOS STATE ----------
+    public void confirmarPartido() {
+        estado.confirmar(this);
     }
 
     public void cancelarPartido() {
-
+        estado.cancelar(this);
     }
 
-    public void confirmarPartido() {
-
+    public void finalizarPartido() {
+        estado.finalizar(this);
     }
+
+    public void setEstado(IEstadoPartido estado) {
+        this.estado = estado;
+    }
+
+    public IEstadoPartido getEstado() {
+        return estado;
+    }
+
+    // --------- MÉTODOS STRATEGY ----------
+    public void setEstrategiaEmparejamiento(IEstrategiaEmparejamiento estrategia) {
+        this.estrategiaEmparejamiento = estrategia;
+    }
+
+    public void buscarPartido() {
+       
+    }
+
+    // --------- GETTERS / SETTERS ----------
+    public Deporte getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(Deporte deporte) {
+        this.deporte = deporte;
+    }
+
+    public List<Usuario> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(List<Usuario> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public Ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(Ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    // --------- TO STRING ----------
+    @Override
+    public String toString() {
+        return "Partido{" +
+                "deporte=" + deporte +
+                ", ubicacion=" + ubicacion +
+                ", comienzo=" + fecha +
+                ", duracionMinutos=" + duracion +
+                ", estado=" + (estado != null ? estado.nombre() : "Sin estado") +
+                '}';
+    }
+
+    // --------- OTROS MÉTODOS VACÍOS ----------
+    public void crearPartido() {}
+
+    public void agregarPartido() {}
 }
--- findamatch/src/main/java/com/findamatch/model/estado/IEstadoPartido.java
+++ findamatch/src/main/java/com/findamatch/model/estado/IEstadoPartido.java
@@ -1,4 +1,10 @@
 package com.findamatch.model.estado;
 
+import com.findamatch.model.Partido;
+
 public interface IEstadoPartido {
+    void confirmar(Partido partido);
+    void cancelar(Partido partido);
+    void finalizar(Partido partido);
+    String nombre();
 }
